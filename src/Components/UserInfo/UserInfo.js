c1337603d1d7b00cb25544a79ba370ffe0079e74
Protik111 <rafiurprotik111.gmail.com>
2021-03-22 17:18:10 +0600

    filtering data  added to a specific user

--- src/Components/UserInfo/UserInfo.js
+++ src/Components/UserInfo/UserInfo.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Nav, Button, Form, FormControl } from 'react-bootstrap';
+import '../Header/Header.css';
+import logo from '../../logos/Group1329.png';
+import { UserContext } from '../../App';
+import { useContext } from 'react';
+import '../UserInfo/UserInfo.css';
 
 const UserInfo = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [event, setEvent] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/showEvent?email='+loggedInUser.email)
+        .then(res => res.json())
+        .then(data => setEvent(data));
+    }, [])
     return (
         <div>
-            <h2>This is User Info</h2>
+            <div className="col-md-12">
+                <Nav className="justify-content-end" id="menu" activeKey="/home">
+                        <div className="col-md-6">
+                            <img className="d-inline-block align-top" src={logo} alt=""/>
+                        </div>
+                    <Nav.Item>
+                        <Nav.Link className="link" href="/home">Home</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link className="link" eventKey="link-1">Donations</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link className="link" eventKey="link-2">Events</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link className="link" eventKey="link-2">Blogs</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item className="loggedName">
+                        <p><strong>{loggedInUser.name}</strong></p>
+                    </Nav.Item>
+                </Nav>
+            </div>
+            <div className="userContainer">
+                <h1>{event.length}</h1>
+                {
+                    event.map(evnt => <p>{evnt.name}</p>)
+                }
+            </div>
         </div>
     );
 };
